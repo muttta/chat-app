6dca333cee1e3122e83255f9bd52e422df14987d
muttta <93360763+muttta@users.noreply.github.com>
2021-11-05 16:26:00 +0900

    画像のみでもメッセージ送信可能となるバリテーションの設定

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,31 +1,31 @@
 class RoomsController < ApplicationController
-  def new
-    @room = Room.new
-  end
 
-  def create
-    @room = Room.new(room_params)
-    if @room.save
-      redirect_to root_path
-    else
-      render :new
-    end
-  end
+def index
 
-  def index
-  end
+end
 
-  def destroy
-    room = Room.find(params[:id])
-    room.destroy
+def new
+  @room = Room.new
+end
+
+def create
+  @room = Room.new(room_params)
+  if @room.save
     redirect_to root_path
+  else
+    render :new
   end
+end
 
+def destroy
+  room = Room.find(params[:id])
+  room.destroy
+  redirect_to root_path
+end
 
-  private
+private
 
-  def room_params
-    params.require(:room).permit(:name, user_ids: [])
-    
-  end
+def room_params
+  params.require(:room).permit(:name, user_ids:[])
+end
 end
